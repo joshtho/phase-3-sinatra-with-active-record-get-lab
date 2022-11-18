7339d44c19103290a3ebbfe963c37c72f896e8f8
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-11-18 11:58:27 -0600

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
+  get '/bakeries' do 
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  get '/bakeries/:id' do 
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+  get '/baked_goods/by_price' do 
+    bakeries = Bakery.order(:price)
+    bakeries.to_json
+  end
 
 end
